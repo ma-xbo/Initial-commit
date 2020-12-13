66b4d75bf733f395debaa5c00bc4775b53a7e4f6
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2020-12-13 09:33:51 +0100

    added first TextInput for testing to NewEntry

--- views/NewEntry.js
+++ views/NewEntry.js
@@ -1,16 +1,54 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, TextInput, StyleSheet, View, Pressable } from 'react-native';
 
 export default function NewEntry() {
+    const [title, setTitle] = useState('');
     return (
-        <View style={styles.container}>
-            <Text>Hello from NewEntry</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Text style={{ alignSelf: 'center' }}>Hello from NewEntry</Text>
+            <View style={styles.midView} >
+                <Text>Titel</Text>
+                <TextInput
+                    placeholder='Titel'
+                    style={styles.textInput}
+                    onChangeText={(val) => setTitle(val)}
+                />
+            </View>
+            <Pressable
+                onPress={() => alert(title)}
+                style={styles.submitButton}
+            >
+                <Text>Button</Text>
+            </Pressable>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'blue'
+        flex: 1,
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        backgroundColor: 'gainsboro',
     },
+    textInput: {
+        backgroundColor: 'white',
+        minWidth: 200,
+        padding: 10,
+        borderRadius: 18,
+        marginHorizontal: 10
+    },
+    midView: {
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        backgroundColor: 'cyan',
+        width:'100%',
+        padding: 10,
+
+    },
+    submitButton: {
+        backgroundColor: 'darkblue',
+        padding: 5
+    }
 });
