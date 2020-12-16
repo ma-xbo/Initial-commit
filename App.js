b975862bd9ce3f573067ce0941b1c21d8038b0c2
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2020-12-16 12:38:59 +0100

    Updated Expo, added Icons to Tabs

--- App.js
+++ App.js
@@ -1,18 +1,58 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import OverviewList from './views/OverviewList';
-import Settings from './views/Settings';
-import NewEntry from './views/NewEntry';
-import Optimization from './views/Optimization';
-import FinanceAnalysis from './views/FinanceAnalysis';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import OverviewList from "./views/OverviewList";
+import Settings from "./views/Settings";
+import NewEntry from "./views/NewEntry";
+import Optimization from "./views/Optimization";
+import FinanceAnalysis from "./views/FinanceAnalysis";
 
 export default function App() {
   const Tab = createBottomTabNavigator();
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        //Vorbereitung für die verwendung von Icons --> Library muss noch ausgesucht und installiert werden
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            switch (route.name) {
+              case "Overview":
+                iconName = focused
+                  ? "ios-list"
+                  : "ios-list-outline";
+                break;
+              case "Optimization":
+                iconName = focused ? "ios-pie-chart" : "ios-pie-chart-outline";
+                break;
+              case "NewEntry":
+                iconName = focused
+                  ? "ios-add-circle"
+                  : "ios-add-circle-outline";
+                size = 30;
+                break;
+              case "Analysis":
+                iconName = focused ? "ios-analytics" : "ios-analytics-outline";
+                break;
+              case "Settings":
+                iconName = focused ? "ios-settings" : "ios-settings-outline";
+                break;
+              default:
+                break;
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "#6097f0",
+          inactiveTintColor: "gray",
+          style: { backgroundColor: "lavender" },
+        }}
+      >
         <Tab.Screen name="Overview" component={OverviewList} />
         <Tab.Screen name="Optimization" component={Optimization} />
         <Tab.Screen name="NewEntry" component={NewEntry} />
