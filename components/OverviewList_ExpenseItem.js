810a6573c12c1174bb747b7b35630d853b7b60dd
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2020-12-17 14:04:32 +0100

    stsyling on OverviewList and Navigation to Details

--- components/OverviewList_ExpenseItem.js
+++ components/OverviewList_ExpenseItem.js
@@ -1,22 +1,39 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const colorDefinitions = require("../assets/colorDefinition.json");
 
 export default function OverviewList_ExpenseItem(props) {
-  const { title } = props;
+  //const { title } = props;
+  const onPress = props.onPress;
+  const title = props.itemObject.title;
+  const amount = props.itemObject.amount;
 
   const text = "Hello from " + title;
+
   return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-    </View>
+    <Pressable style={styles.container} onPress={onPress}>
+      <Ionicons
+        name="ios-construct"
+        size={25}
+        color={colorDefinitions.light.black}
+        style={{ paddingRight: 8 }}
+      />
+      <View>
+        <Text>{text}</Text>
+      </View>
+    </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
-    backgroundColor: "cyan",
-    margin: 2,
+    flexDirection: "row",
+    backgroundColor: colorDefinitions.light.teal,
+    marginVertical: 4,
+    marginHorizontal: 6,
     padding: 8,
+    borderRadius: 5,
   },
 });
--- views/OverviewList.js
+++ views/OverviewList.js
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList, Pressable } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import AppSafeAreaView from "../components/AppSafeAreaView";
 import OverviewList_ExpenseItem from "../components/OverviewList_ExpenseItem";
+const colorDefinitions = require("../assets/colorDefinition.json");
 
 export default function OverviewList() {
   const Stack = createStackNavigator();
   return (
     <Stack.Navigator
       screenOptions={{
-        headerStyle: { backgroundColor: "lavender" },
+        headerStyle: { backgroundColor: colorDefinitions.light.black },
+        headerTintColor: colorDefinitions.light.white,
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
       }}
     >
-      <Stack.Screen name="Übersicht" component={List} />
+      <Stack.Screen
+        name="Übersicht"
+        component={List}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen name="Details" component={Details} />
     </Stack.Navigator>
   );
@@ -26,14 +35,21 @@ function List(props) {
       <View
         style={{
           width: "100%",
-          backgroundColor: "#fff",
+          backgroundColor: colorDefinitions.light.gray6,
         }}
       >
         <FlatList
           data={DATA}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
-            <OverviewList_ExpenseItem title={item.title} />
+            <OverviewList_ExpenseItem
+              itemObject={item}
+              onPress={() =>
+                navigation.navigate("Details", {
+                  itemObject: JSON.stringify(item),
+                })
+              }
+            />
           )}
         />
       </View>
@@ -41,11 +57,20 @@ function List(props) {
   );
 }
 
-function Details({ navigation, route }) {
-  const id = route.params.id;
+function Details(props) {
+  const route = props.route;
+  const item = JSON.parse(route.params.itemObject);
+
   return (
     <View style={styles.container}>
-      <Text>{"Details" + id}</Text>
+      <Text>{"Title " + item.title}</Text>
+      <Text>{"Amount " + item.amount}</Text>
+      <Pressable>
+        <Text>Edit</Text>
+      </Pressable>
+      <Pressable>
+        <Text>Delete</Text>
+      </Pressable>
     </View>
   );
 }
@@ -58,13 +83,34 @@ const DATA = [
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
     title: "First Item",
+    description: "Description 1",
+    amount: 100,
+    date: new Date(),
+    createdBy: "Max",
+    createdAt: new Date(),
+    modifiedBy: "Max",
+    modifiedAt: new Date(),
   },
   {
     id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
     title: "Second Item",
+    description: "Description 2",
+    amount: -136,
+    date: new Date(),
+    createdBy: "Max",
+    createdAt: new Date(),
+    modifiedBy: "Max",
+    modifiedAt: new Date(),
   },
   {
     id: "58694a0f-3da1-471f-bd96-145571e29d72",
     title: "Third Item",
+    description: "Description 3",
+    amount: 82374,
+    date: new Date(),
+    createdBy: "Max",
+    createdAt: new Date(),
+    modifiedBy: "Max",
+    modifiedAt: new Date(),
   },
 ];
