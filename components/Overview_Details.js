abbe26dda9d55860934381855bbd8d5561232fdb
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2020-12-20 11:23:44 +0100

    changed Buttons in Details Screen

--- components/Overview_Details.js
+++ components/Overview_Details.js
@@ -9,18 +9,92 @@ export default function Overview_Details(props) {
 
   return (
     <View style={styles.container}>
-      <Text>{"Title " + item.title}</Text>
-      <Text>{"Amount " + item.amount}</Text>
-      <Pressable>
-        <Text>Edit</Text>
-      </Pressable>
-      <Pressable>
-        <Text>Delete</Text>
-      </Pressable>
+      <View style={styles.contentContainer}>
+        <Text>{"Title " + item.title}</Text>
+        <Text>{"Amount " + item.amount}</Text>
+      </View>
+      <View style={styles.bottomContainer}>
+        <View style={styles.buttonContainer}>
+          <Pressable
+            style={[
+              styles.controlButton,
+              { backgroundColor: colorDefinitions.light.green },
+            ]}
+          >
+            <Text style={styles.controlButtonText}>Edit</Text>
+          </Pressable>
+          <Pressable
+            style={[
+              styles.controlButton,
+              { backgroundColor: colorDefinitions.light.red },
+            ]}
+          >
+            <Text style={styles.controlButtonText}>Delete</Text>
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  dummy: {},
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+  },
+  bottomContainer: {
+    width: "100%",
+    position: "absolute",
+    bottom: 0,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginVertical: 10,
+  },
+  controlButton: {
+    alignItems: "center",
+    width: 150,
+    padding: 6,
+    marginVertical: 1,
+    borderRadius: 5,
+  },
+  controlButtonText: {
+    fontSize: 20,
+    color: colorDefinitions.light.white,
+  },
 });
+
+/* 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+  },
+  bottom: {
+    height: 100,
+  },
+  buttonContainer: {
+    height:100,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    margin: 5,
+    backgroundColor: "cyan",
+  },
+  controlButton: {
+    alignItems: "center",
+    width: 150,
+    padding: 6,
+    borderRadius: 5,
+  },
+  controlButtonText: {
+    fontSize: 20,
+  },
+});
+ */
