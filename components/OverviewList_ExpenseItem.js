cfd4463450b509fe350bd2cbc8d1141922623fc8
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2020-12-24 12:28:38 +0100

    Added Currency to ExpenseItem

--- components/OverviewList_ExpenseItem.js
+++ components/OverviewList_ExpenseItem.js
@@ -1,32 +1,68 @@
 import React from "react";
 import { View, Text, StyleSheet, Pressable } from "react-native";
 import PaymentMethodIcon from "./PaymentMethodIcon";
-
 const colorDefinitions = require("../assets/colorDefinition.json");
 
 export default function OverviewList_ExpenseItem(props) {
   const { onPress, itemObject } = props;
-  const { title, amount, paymentMethod } = itemObject;
-
-  const text = "Hello from " + title;
+  const { title, description, amount, paymentMethod, currency } = itemObject;
 
   return (
     <Pressable style={styles.container} onPress={onPress}>
       <PaymentMethodIcon paymentMethod={paymentMethod} />
       <View>
-        <Text>{text}</Text>
+        <Text>{title}</Text>
+        <Text>{description}</Text>
       </View>
+      <AmountText value={amount} currency={currency} />
     </Pressable>
   );
+}
+
+function AmountText(props) {
+  const { value, currency } = props;
+
+  if (value < 0) {
+    return (
+      <View style={{ flexDirection: "row" }}>
+        <Text style={[styles.textStyle, { color: colorDefinitions.light.red }]}>
+          {value}
+        </Text>
+        <Text style={[styles.textStyle, { color: colorDefinitions.light.red }]}>
+          {currency}
+        </Text>
+      </View>
+    );
+  } else {
+    return (
+      <View style={{ flexDirection: "row" }}>
+        <Text
+          style={[styles.textStyle, { color: colorDefinitions.light.green }]}
+        >
+          {value}
+        </Text>
+        <Text
+          style={[styles.textStyle, { color: colorDefinitions.light.green }]}
+        >
+          {currency}
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    backgroundColor: colorDefinitions.light.teal,
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: colorDefinitions.light.black,
     marginVertical: 4,
     marginHorizontal: 6,
     padding: 8,
     borderRadius: 5,
+  },
+  textStyle: {
+    fontSize: 15,
   },
 });
