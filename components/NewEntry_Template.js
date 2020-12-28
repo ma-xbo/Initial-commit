4bb0ea2939acea14b941f8b6d806664a59792d47
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2020-12-28 23:33:13 +0100

    random colors to teamplates

--- components/NewEntry_Template.js
+++ components/NewEntry_Template.js
@@ -1,32 +1,50 @@
-import React, { useState } from "react";
-import {
-  SafeAreaView,
-  Text,
-  TextInput,
-  StyleSheet,
-  View,
-  Pressable,
-} from "react-native";
+import React from "react";
+import { Text, TextInput, StyleSheet, View, Pressable } from "react-native";
 const colorDefinitions = require("../assets/colorDefinition.json");
 
 export default function NewEntry_Template(props) {
   const { text } = props;
 
   return (
-    <Pressable style={styles.container}>
+    <Pressable style={[styles.container, { backgroundColor: randomColor() }]}>
       <Text>{text}</Text>
     </Pressable>
   );
 }
 
 function randomColor() {
-  return colorDefinitions.light.blue;
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+  const allColors = Object.keys(colorDefinitions.light);
+  const excludedColors = [
+    "white",
+    "black",
+    "gray",
+    "gray2",
+    "gray3",
+    "gray4",
+    "gray5",
+    "gray6",
+  ];
+
+  const filteredColors = allColors.filter(
+    (item) => !excludedColors.includes(item)
+  );
+
+  return filteredColors[getRandomInt(filteredColors.length)];
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    justifyContent: "center",
     padding: 10,
-    backgroundColor: randomColor,
-    borderRadius: 5,
+    borderRadius: 10,
+    width: 90,
+    height: 90,
+    margin: 5,
   },
 });
