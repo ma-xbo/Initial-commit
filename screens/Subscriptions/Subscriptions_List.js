408e43996bd145336934a208bed7caee4a2840ec
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2021-01-09 20:16:01 +0100

    add SubscriptionItem to Flatlist, manage data for SubscriptionItem

--- screens/Subscriptions/Subscriptions_List.js
+++ screens/Subscriptions/Subscriptions_List.js
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import AppSafeAreaView from "../../components/AppSafeAreaView";
+import SubscriptionItem from "../../components/SubscriptionItem";
+const dummyData = require("../../assets/dummyData.json");
 
 export default function Settings() {
+  const navigation = props.navigation;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const subData = [];
+
+    const dataArray = dummyData.map((item) => {
+      let rObj = { ...item };
+      rObj["date"] = new Date(item.date);
+      rObj["createdAt"] = new Date(item.createdAt);
+      rObj["modifiedAt"] = new Date(item.modifiedAt);
+      return rObj;
+    });
+
+    for (let index = 0; index < dataArray.length; index++) {
+      const element = dataArray[index];
+      if (element.isSubscription === true) {
+        subData.push(element);
+      }
+    }
+    subData.sort(function (a, b) {
+      return new Date(b.date) - new Date(a.date);
+    });
+
+    setData(subData);
+  }, []);
+
   return (
-    <View>
+    <AppSafeAreaView title="Abonnements">
       <Text>Test</Text>
-    </View>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <SubscriptionItem
+            itemObject={item}
+            onPress={
+              () => alert("navigate")
+              /*                 navigation.navigate("Details", {
+                  itemObject: JSON.stringify(item),
+                }) */
+            }
+          />
+        )}
+      />
+    </AppSafeAreaView>
   );
 }
 
