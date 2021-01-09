02986fe4a39e3e28c3d9b850edfcf43c69b954fc
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2021-01-09 19:45:42 +0100

    Add AppSafeAreaView to Analysis Screen

--- screens/Analysis.js
+++ screens/Analysis.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import AppSafeAreaView from "../components/AppSafeAreaView";
+
+/* https://github.com/indiespirit/react-native-chart-kit */
 
 export default function FinanceAnalysis() {
-    return (
-        <View style={styles.container}>
-            <Text>Hello from FinanceAnalysis</Text>
-        </View>
-    );
+  return (
+    <AppSafeAreaView title="Analyse">
+      <View style={styles.container}>
+        <Text>Heatmap zu den Ausgaben (pro Datum)</Text>
+        <Text>Komponente kommt hier</Text>
+        <Text>Ausgaben pro Kategorie</Text>
+        <Text>Komponente kommt hier</Text>
+        <Text>Ausgaben pro Geschäft</Text>
+        <Text>Komponente kommt hier</Text>
+      </View>
+    </AppSafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'red'
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+  },
 });
