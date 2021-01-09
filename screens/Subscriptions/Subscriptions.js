7d0f83d644277c5b024bb62c8d12c43c5aa2b5ad
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2021-01-09 19:58:08 +0100

    Add new components for Subscription and Subscription_list

---
+++ screens/Subscriptions/Subscriptions.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Subscriptions_List from "./Subscriptions_List";
+const colorDefinitions = require("../assets/colorDefinition.json");
+
+export default function OverviewList() {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: colorDefinitions.light.black },
+        headerTintColor: colorDefinitions.light.white,
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="Abos"
+        component={Subscriptions_List}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
---
+++ screens/Subscriptions/Subscriptions_List.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Settings() {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "blue",
+  },
+});
