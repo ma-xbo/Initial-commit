2e731029693e03a578c4cfdd20c71f313a65e1c8
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2021-01-10 10:11:56 +0100

    added conditional formating to NumberInput

--- components/NumberInput.js
+++ components/NumberInput.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TextInput, StyleSheet, Text, View } from "react-native";
 import { Picker } from "@react-native-picker/picker";
-import { useEffect } from "react/cjs/react.development";
 
 export default function NumberInput(props) {
   const {
@@ -15,12 +14,7 @@ export default function NumberInput(props) {
     containerStyle,
   } = props;
   const numbers = [0, 1, 2, 3, 4, 5, 6];
-  const [valueInput, setValueInput] = useState(0);
-
-  const [inputIsFocused, setInputIsFocused] = useState(false);
-  const [selectedNumber, setSelectedNumber] = useState(numbers[0]);
-
-  console.log(value);
+  const [valueInput, setValueInput] = useState(0.0);
 
   useEffect(() => {
     setValueInput(parseFloat(valueInput).toFixed(2));
@@ -31,6 +25,10 @@ export default function NumberInput(props) {
       onChangeValue(parseFloat(valueInput).toFixed(2));
     }
   }, [isNegativ]);
+
+  useEffect(() => {
+    setValueInput(parseFloat(Math.abs(value)).toFixed(2));
+  }, [value]);
 
   const onChanged = (text) => {
     let amount = text.replace(/[^0-9]/g, "");
@@ -58,13 +56,18 @@ export default function NumberInput(props) {
   };
 
   return (
-    <View style={[styles.containerStyle, containerStyle]}>
+    <View
+      style={[
+        isNegativ
+          ? styles.containerStyleNegativ
+          : styles.containerStylePositive,
+        containerStyle,
+      ]}
+    >
       {isNegativ && <Text style={styles.textStyle}>-</Text>}
       {true && (
         <View style={{ flexDirection: "row", justifyContent: "flex-end" }}>
           <TextInput
-            onFocus={() => setInputIsFocused(true)}
-            onBlur={() => setInputIsFocused(false)}
             placeholder={placeholderText}
             keyboardType="number-pad"
             style={styles.textStyle}
@@ -75,7 +78,7 @@ export default function NumberInput(props) {
         </View>
       )}
 
-      {false && (
+      {/*       {false && (
         <Picker
           style={{ height: 50, width: 200, marginBottom: 150 }}
           selectedValue={selectedNumber}
@@ -88,16 +91,26 @@ export default function NumberInput(props) {
             <Picker.Item key={index} label={val.toString()} value={val} />
           ))}
         </Picker>
-      )}
+      )} */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  containerStyle: {
+  containerStylePositive: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    flexBasis: "",
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 15,
+  },
+  containerStyleNegativ: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
+    flexBasis: "",
     backgroundColor: "white",
     padding: 10,
     borderRadius: 15,
