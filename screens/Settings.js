0d8a8e57c17c6f4046ebdb5a45958754d1732924
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2021-01-14 19:38:17 +0100

    Add logout button to Settings View

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,16 +1,52 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import AppSafeAreaView from "../components/AppSafeAreaView";
+const colorDefinitions = require("../assets/colorDefinition.json");
 
-export default function Settings() {
-    return (
-        <View style={styles.container}>
-            <Text>Hello from Settings</Text>
-        </View>
-    );
+export default function Settings(props) {
+  const navigation = props.navigation;
+
+  return (
+    <AppSafeAreaView title="Einstellungen">
+      <View style={styles.container}>
+        <Text>Hello from Settings</Text>
+        <Pressable
+          style={styles.logoutButton}
+          onPress={() => navigation.navigate("Login")}
+        >
+          <Text style={styles.logoutText}>Logout</Text>
+        </Pressable>
+      </View>
+    </AppSafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'blue'
+  container: {
+    flex: 1,
+    width: "100%",
+  },
+  logoutButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: colorDefinitions.light.gray6,
+    borderTopColor: colorDefinitions.light.black,
+    borderBottomColor: colorDefinitions.light.black,
+    borderTopWidth: 0.2,
+    borderBottomWidth: 0.2,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  logoutText: {
+    fontSize: 18,
+    color: colorDefinitions.light.red,
+  },
 });
