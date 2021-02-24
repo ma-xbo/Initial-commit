31eb6496b07e5e84610995a237967f6408816103
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2021-02-24 18:56:41 +0100

    Delete Entry working for Redux and Firebase

--- js/redux/actions/Finance.js
+++ js/redux/actions/Finance.js
@@ -1,4 +1,9 @@
-import { LOAD_FINANCEDATA, ADD_FINANCEDP } from "../actionTypes.js";
+import {
+  LOAD_FINANCEDATA,
+  ADD_FINANCEDP,
+  DELETE_FINANCEDP,
+  UPDATE_FINANCEDP,
+} from "../actionTypes.js";
 
 export function loadFinanceData(dataArray) {
   return {
@@ -10,6 +15,20 @@ export function loadFinanceData(dataArray) {
 export function addFinanceItem(datapoint) {
   return {
     type: ADD_FINANCEDP,
+    payload: datapoint,
+  };
+}
+
+export function deleteFinanceItem(datapointId) {
+  return {
+    type: DELETE_FINANCEDP,
+    payload: datapointId,
+  };
+}
+
+export function updateFinanceItem(datapoint) {
+  return {
+    type: UPDATE_FINANCEDP,
     payload: datapoint,
   };
 }
